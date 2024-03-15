7ab9e286f2af6ec2ec4ec072b1774a73b3853b7e
Alvaro1102porto <alvaroporto.1102@gmail.com>
2024-03-15 08:23:22 -0300

    variavel_nome

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 
-function alertar(event){
-    alert("Você clicou no botão! " + event);
+    var nome = document.getElementById("nome");
+    function alertar(event){
+
+        alert("Você clicou no botão! " + nome.value);
 
     var numero =6;
     var resultado = numero % 2;
     if(resultado == 0){
-            alert("este número é par!");
+            
+        alert("este número é par!");
     }
 }
 
